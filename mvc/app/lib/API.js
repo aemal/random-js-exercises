214d5faf972fedc0b7b2dfeac976cea3c5367fd2
Aemal Sayer <aemalsayer@aemals-air.localdomain>
2018-03-05 17:00:29 +0100

    initial commit

---
+++ mvc/app/lib/API.js
@@ -0,0 +1,16 @@
+class API {
+    constructor() {
+        this.API_URL = 'https://barkwire-api.herokuapp.com/dogs';
+
+    }
+
+    getDogs() {
+        return fetch(this.API_URL)
+            .then(res => res.json());
+    }
+
+    getDog(id) {
+        return fetch(`${this.API_URL}/${id}`)
+            .then(res => res.json());
+    }
+}
