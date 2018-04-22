e13138eaee8edc4a3e31982472bba8864a6d02e1
Aemal Sayer <aemalsayer@aemals-air.localdomain>
2018-04-22 16:49:59 +0200

    added publish npm-package example

---
+++ random-js-examples/publish-npm-packages/npm-package-hello-world/README.md
@@ -0,0 +1,2 @@
+# This is a hello world package!!! :D
+This can be removed later
---
+++ random-js-examples/publish-npm-packages/test-package/index.js
@@ -0,0 +1,4 @@
+const { area, perimeter } = require('rgc-todo-list');
+
+console.log(area(200));
+console.log(perimeter(200));
