a12817662f0f7c791c0653916d61b6f9e3a42c8b
aemal <a@reactgeeks.com>
2018-05-05 02:34:59 +0500

    Star component added

--- react-codeChallenges/product-review/src/components/Reviews/Review.js
+++ react-codeChallenges/product-review/src/components/Reviews/Review.js
@@ -1,5 +1,18 @@
-import React from 'react'
+import React, {Fragment} from 'react'
+
+// Libs
+import StarRatingComponent from 'react-star-rating-component';
 
 export const Review = ({review}) => {
-  return <h4>Title: {review.title}</h4>
+  return (
+    <Fragment>
+        <h4>Title: {review.title}</h4>
+        <StarRatingComponent
+          name="rating"
+          editing={false}
+          starCount={5}
+          value={review.stars}
+        />
+    </Fragment>
+  )
 }
