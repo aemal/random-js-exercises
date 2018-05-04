926d98c095b543b5a150d5471687036b9b8b1706
aemal <a@reactgeeks.com>
2018-05-05 02:39:55 +0500

    date added momentjs

--- react-codeChallenges/product-review/src/components/Reviews/Review.js
+++ react-codeChallenges/product-review/src/components/Reviews/Review.js
@@ -2,6 +2,7 @@ import React, {Fragment} from 'react'
 
 // Libs
 import StarRatingComponent from 'react-star-rating-component';
+import Moment from 'react-moment';
 
 export const Review = ({review}) => {
   return (
@@ -13,6 +14,9 @@ export const Review = ({review}) => {
           starCount={5}
           value={review.stars}
         />
+        <p>Date: 
+        <Moment format="MM/DD/YYYY">{review.reviewCreated}</Moment>
+        </p>
     </Fragment>
   )
 }
