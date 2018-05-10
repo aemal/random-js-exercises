915ff4407ccbcb7c5903aedbd4e6993a506c25d6
Aemal Sayer <aemalsayer@aemals-air.localdomain>
2018-05-10 11:39:02 +0200

    added react-sass example

---
+++ react-examples/react-sass-example/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './css/style.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="heading-primary--main">
+          Hello World!
+        </div>
+        <div className="heading-primary--sub">
+          Bye World!
+        </div>
+      </div>
+
+    );
+  }
+}
+
+export default App;
