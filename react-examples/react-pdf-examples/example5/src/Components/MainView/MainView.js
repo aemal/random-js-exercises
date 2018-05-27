371de079d05ff685f3031aaa1eb17c313a4562ef
alimudassar111 <alimudassar111@live.com>
2018-05-28 01:15:41 +0500

    removed extra logging to console

--- react-examples/react-pdf-examples/example5/src/Components/MainView/MainView.js
+++ react-examples/react-pdf-examples/example5/src/Components/MainView/MainView.js
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types'
 // import Fuck  from 'pdfjs-dist/build/pdf.min';
 
-import {TextLayerBuilder, PDFViewerApplication} from 'pdfjs-dist/web/pdf_viewer.js'
+import { TextLayerBuilder } from 'pdfjs-dist/web/pdf_viewer.js'
 
 export  class MainView extends Component {
   constructor(){
@@ -67,7 +67,6 @@ export  class MainView extends Component {
     this.clearChildNodes(Div)
     let mainCanvas = this.mainCanvas.current
     this.clearChildNodes(mainCanvas)
-    console.log(this.props.pdfDocument)
     this.props.pdfDocument.getPage(currentPage).then(page => {
         let scale = this.props.scale
         let viewport = page.getViewport(scale);
